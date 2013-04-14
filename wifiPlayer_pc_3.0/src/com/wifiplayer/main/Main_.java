fea9ace2b6970355b2d59884afaa0acb8dfabeb7
chenkaigang <chenkaigang@bogon>
2013-04-14 09:40:09 +0800

    测试pc版

---
+++ wifiPlayer_pc_3.0/src/com/wifiplayer/main/Main_.java
@@ -0,0 +1,17 @@
+package com.wifiplayer.main;
+/**
+ * 软件启动类
+ * @author chenkaigang
+ *
+ */
+public class Main_ {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
